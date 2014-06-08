b1f8d44dc58ac8680e76450c21acdb5995518b49
Edoardo Morassutto <gianluigi988@gmail.com>
2014-06-08 18:27:52 +0200

    Aggiornata routes.rb
    Aggiunti label per i percorsi più comuni

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,23 @@
 Rails.application.routes.draw do
 
-  root :to => 'evaluations#index'
+	# TODO edit to get to the application controller
+	root :to => 'evaluations#index'
 
-	get '/evaluations' => 'evaluations#index'
+	get '/evaluations' => 'evaluations#index', as: 'evaluations'
 	get '/evaluations/show' => 'evaluations#show'
 	get '/evaluations/show/:id' => 'evaluations#show'
-	get '/evaluations/:id' => 'evaluations#show'
+	get '/evaluations/:id' => 'evaluations#show', as: 'evaluation'
 	
 	get '/evaluations/class/:id' => 'evaluations#show_class'
 	get '/evaluations/user/:id' => 'evaluations#show_user'
 	
-	get '/evaluations/:id/edit' => 'evaluations#edit'
+	get '/evaluations/:id/edit' => 'evaluations#edit', as: 'edit_evaluation'
 	put '/evaluations/:id/edit' => 'evaluations#edit'
 	patch '/evaluations/:id/edit' => 'evaluations#edit'
 	
 	delete '/evaluations/:id/delete' => 'evaluations#delete'
 	
-	get '/evaluations/new' => 'evaluations#new'
+	get '/evaluations/new' => 'evaluations#new', as: 'new_evaluation'
 	post '/evaluations/new' => 'evaluations#new'
 	
 	get '/evaluations/mult' => 'evaluations#mult'
