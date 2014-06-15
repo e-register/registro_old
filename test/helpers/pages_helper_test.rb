e19bc5972190b334185422d086c070a2a304b23d
Edoardo Morassutto <gianluigi988@gmail.com>
2014-06-15 17:06:12 +0200

    Aggiunti testcase

--- test/helpers/pages_helper_test.rb
+++ test/helpers/pages_helper_test.rb
@@ -1,4 +1,22 @@
 require 'test_helper'
 
 class PagesHelperTest < ActionView::TestCase
+	test "Get type guest" do
+		assert_equal :guest, get_user_type(nil)
+	end
+	
+	test "Get type student" do
+		edoardo = users(:user_edoardo)
+		assert_equal :student, get_user_type(edoardo.id)
+	end
+	
+	test "Get type teacher" do
+		prof = users(:user_prof)
+		assert_equal :teacher, get_user_type(prof.id)
+	end
+	
+	test "Get type admin" do
+		admin = users(:user_admin)
+		assert_equal :admin, get_user_type(admin.id)
+	end
 end
