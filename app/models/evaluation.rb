4253237c5f2f4920151db3230f6c5fc44cd1d99b
Edoardo Morassutto <gianluigi988@gmail.com>
2014-06-15 17:49:52 +0200

    Aggiunte funzioni per estrarre i voti di una classe o di un utente

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -13,4 +13,19 @@ class Evaluation < ActiveRecord::Base
 	belongs_to :score
 	# the evaluation may be in a class test group
 	belongs_to :class_test
+	
+	# return all the evaluations of a class
+	def self.get_from_class class_info
+		students = class_info.get_students.map { |e| e.student_id }
+		evaluations = Evaluation.where("student_id IN (?)", students).to_a
+		# TODO implement permission control!
+		return evaluations		
+	end
+	
+	# return all the evaluation of an user
+	def self.get_from_user user
+		evaluations = Evaluation.where("student_id = ? OR teacher_id = ?", user.id, user.id).to_a
+		# TODO implement permission control!
+		return evaluations
+	end
 end
